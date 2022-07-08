3ceffe9a5e219b23ca9a82f084407f3c9fb600c7
KarapetyanStenik <KarapetyanStenik99@gmail.com>
2022-07-08 22:53:38 +0400

    WEBSOCKETS
    Delete patterns.md
    
    solid

---
+++ 1.SoftwarePrinciples/5.Solid/SolidTutorial/common.md
@@ -0,0 +1,36 @@
+## Solid, What it is?
+
+We all know designing software takes a lot of work. It's not often clear what constitutes a good design. The solid design principles are five ideas that can help build software that's easy to extend and adapt.
+
+## Class
+
+In an O, O programming language like Python, 
+class definitions can be used to 
+1. confine the ripples of change.
+2. A class allows programmers to isolate the various concerns that comprise working software(separation of concerns)
+ - For example, we can disentangle algorithms and data structures from the operating system details.
+
+ The SOLID Design Principles provide guidance on how to structure our classes in a way that keeps the concerns separate and minimizes the ripples of change.
+
+I want to summarize the SOLID design principles before digging into the details of each principle. Here are the overall goals for SOLID. 
+First to prevent problems by giving explicit guidance on design problems that we may not always anticipate.
+Second to distinguish style from substance.
+There are fundamental design approaches that will lead to better software. 
+Here are the five design principles that form a handy acronym, 
+1. single responsibility, 
+2. open/closed, 
+3. Liskov substitution, 
+4. interface segregation, 
+5. dependency inversion. 
+It's important to note these five aren't the only OO design principles. I'll touch on some others. 
+
+
+These five software development principles are guidelines to follow when building software so that it is easier to scale and maintain. They were made popular by a software engineer, Robert C. Martin.
+You see, some of these principles may look similar but they are not targeting the same goal. It is possible to satisfy one principle while violating the other, even though they are alike.
+
+
+## Conclusion
+
+SOLID design principles increases maintainability, scalability, reusability and testability. These principles are global and apply everywhere.
+
+Implementing SOLID design principles during development will lead to systems that are more maintainable, scalable, testable, and reusable. In the current environment, these principles are used globally by engineers. We can use these principles to create beautiful projects that are understandable to industry standards.
--- index.js
+++ index.js
@@ -1,21 +1,164 @@
-// constant
-const PI = 3.14; 
 
-/**
- * Function to calculate area of a circle with given radius r
- * @param {number} r
- * @returns {number} area of circle 
- */
-const calculateArea = r => PI * r * r;
+let eventCalendarBody = {
+    eventTypeCode: "OWG2022",
+    events: [
+      {
+        publisherId: "testPublisherId",
+        clientEventId: "140756822231",
+        eventTypeCode: "CorporateEarnings",
+        eventStatus: {
+          name: "InProgress",
+          testField: "testField",
+        },
+        eventBody: {
+          briefAvailable: "true",
+          presentationAvailable: "true",
+          transcriptAvailable: "false",
+          guidanceAvailable: "false",
+          mediaAvailable: "false",
+          EventData: {
+            Derived: {
+              OrganizationPrimaryRIC: ["CSCO.OQ"],
+              EventDate: "2020-02-27T11:47:04+0000",
+              LastUpdatedAt: "2020-02-27T12:36:02+0000",
+              EventEndDate: "2020-02-28T11:47:04+0000",
+              OrganizationID: ["5037613143"],
+            },
+            Event: {
+              DistinguishingEventName: "Q3 2019 Cisco Inc Earnings Release",
+            },
+            id: "140756811131",
+          },
+        },
+        eventLocation: {
+          country: "Canada",
+          city: "Toronto",
+        },
+        eventSlug: "TEST-SLUG",
+        startDate: "2020-02-28T13:30:00Z",
+        endDate: "2020-02-28T13:30:00Z",
+      },
+    ],
+  };
+  
+  let xmlParsedBodyToJson = {
+    OdfBody: {
+      $: {
+        CompetitionCode: "OWG2022",
+        DocumentCode: "TRU-------------------------------",
+        DocumentType: "DT_SCHEDULE_UPDATE",
+        Version: "2688",
+        Language: "ENG",
+        FeedFlag: "P",
+        Date: "2022-02-17",
+        Time: "084527763",
+        LogicalDate: "2022-02-17",
+        Source: "ODS",
+      },
+      Competition: [
+        {
+          $: {
+            Gen: "WOG-2022-GEN",
+            Codes: "WOG-2022-3.10",
+          },
+          Unit: [
+            {
+              $: {
+                Code: "TRU-IHO-------------------3D------",
+                PhaseType: "2",
+                ScheduleStatus: "SCHEDULED",
+                StartDate: "2022-02-17T12:00:00+08:00",
+                HideStartDate: "N",
+                EndDate: "2022-02-17T13:00:00+08:00",
+                HideEndDate: "N",
+                Venue: "NIS",
+                Location: "NIS",
+                MediaAccess: "OPE",
+                ModificationIndicator: "U",
+                SessionCode:"BOB05"
+              },
+              ItemName: [
+                {
+                  $: {
+                    Language: "ENG",
+                    Value: "Men's Practice Men's Pre-Game SVK",
+                  },
+                },
+                {
+                  $: {
+                    Language: "CHI",
+                    Value: "Men's Practice Men's Pre-Game",
+                  },
+                },
+              ],
+              ItemDescription: [
+                {
+                  _: "<p>M SF1 (Away) MNA SVK</p>",
+                  $: {
+                    Language: "ENG",
+                  },
+                },
+                {
+                  _: "<p>M SF (TBA) MNA</p>",
+                  $: {
+                    Language: "SPA",
+                  },
+                },
+              ],
+              VenueDescription: [
+                {
+                  $: {
+                    VenueName: "National Indoor Stadium",
+                    LocationName: "National Indoor Stadium",
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  };
+  
 
-
-
-/**
- * Function to calculate circumference of a circle with given radius r
- * @param {number} r
- * @returns {number} circumference of circle 
- */
-const calculateCircumference = r => 2 * PI * r;
-
-// logging the contents of module object
-console.log(module);
+  const missingProperties = {
+    eventTypeCode: "OWG2022",
+    events: [
+      {
+        publisherId: undefined,
+        clientEventId: "140756822231",
+        eventTypeCode: "CorporateEarnings",
+        eventStatus: {
+          name: undefined,
+          testField: undefined,
+        },
+        eventBody: {
+          briefAvailable: undefined,
+          presentationAvailable: undefined,
+          transcriptAvailable: undefined,
+          guidanceAvailable: undefined,
+          mediaAvailable: undefined,
+          EventData: {
+            Derived: {
+              OrganizationPrimaryRIC: undefined,
+              EventDate: "2020-02-27T11:47:04+0000",
+              LastUpdatedAt: "2020-02-27T12:36:02+0000",
+              EventEndDate: "2020-02-28T11:47:04+0000",
+              OrganizationID: undefined,
+            },
+            Event: {
+              DistinguishingEventName: "Q3 2019 Cisco Inc Earnings Release",
+            },
+            id: undefined,
+          },
+        },
+        eventLocation: {
+          country: undefined,
+          city: undefined,
+        },
+        eventSlug: "TEST-SLUG",
+        startDate: "2020-02-28T13:30:00Z",
+        endDate: "2020-02-28T13:30:00Z",
+      },
+    ],
+  };
