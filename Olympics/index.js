0f26f2cfffe5ced0f92020e92d8fd8b7ffed44a5
KarapetyanStenik <KarapetyanStenik99@gmail.com>
2022-08-14 15:20:43 +0400

    code

---
+++ Olympics/index.js
@@ -0,0 +1,78 @@
+
+let payload = {
+  OdfBody: {
+    $: {
+      CompetitionCode: "OWG2022",
+      DocumentCode: "TRU-------------------------------",
+      DocumentType: "DT_SCHEDULE_UPDATE",
+      Version: "2688",
+      Language: "ENG",
+      FeedFlag: "P",
+      Date: "2022-02-17",
+      Time: "084527763",
+      LogicalDate: "2022-02-17",
+      Source: "ODS",
+    },
+    Competition: [
+      {
+        $: {
+          Gen: "WOG-2022-GEN",
+          Codes: "WOG-2022-3.10",
+        },
+        Unit: [
+          {
+            $: {
+              Code: "TRU-IHO-------------------3D------",
+              PhaseType: "2",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-02-17T12:00:00+08:00",
+              HideStartDate: "N",
+              EndDate: "2022-02-17T13:00:00+08:00",
+              HideEndDate: "N",
+              Venue: "NIS",
+              Location: "NIS",
+              MediaAccess: "OPE",
+              ModificationIndicator: "U",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Practice Men's Pre-Game SVK",
+                },
+              },
+              {
+                $: {
+                  Language: "CHI",
+                  Value: "Men's Practice Men's Pre-Game",
+                },
+              },
+            ],
+            ItemDescription: [
+              {
+                _: "<p>M SF1 (Away) MNA SVK</p>",
+                $: {
+                  Language: "ENG",
+                },
+              },
+              {
+                _: "<p>M SF (TBA) MNA</p>",
+                $: {
+                  Language: "SPA",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Indoor Stadium",
+                  LocationName: "National Indoor Stadium",
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
