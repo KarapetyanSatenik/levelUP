7d5d441220e152a42f2160c6292f397a76821c35
KarapetyanSatenik <karapetyansatenik99@gmail.com>
2022-10-25 16:20:59 +0400

    new

--- Olympics/date.js
+++ Olympics/date.js
@@ -2,72 +2,52 @@ let xmlParsedBodyToJson = {
   OdfBody: {
     $: {
       CompetitionCode: "OWG2022",
-      DocumentCode: "TRU-------------------------------",
-      DocumentType: "DT_SCHEDULE_UPDATE",
-      Version: "2688",
-      Language: "ENG",
+      DocumentCode: "HIP-------------------------------",
+      DocumentType: "DT_SCHEDULE",
+      Version: "3",
       FeedFlag: "P",
-      Date: "2022-02-17",
-      Time: "084527763",
-      LogicalDate: "2022-02-17",
+      Date: "2022-03-21",
+      Time: "115021245",
+      LogicalDate: "2022-03-21",
       Source: "ODS",
     },
     Competition: [
       {
-        $: {
-          Gen: "WOG-2022-GEN",
-          Codes: "WOG-2022-3.10",
-        },
         Session: [
           {
             $: {
-              SessionCode: "PBTH01",
-              StartDate: "2022-03-05T10:00:00+08:00",
-              EndDate: "2022-03-05T15:05:00+08:00",
-              Venue: "ZBT",
-              VenueName: "Zhangjiakou National Biathlon Centre",
+              SessionCode: "PIH987",
+              StartDate: "2022-02-06T20:05:00+08:00",
+              EndDate: "2022-03-06T22:05:00+08:00",
+              Leadin: "0:00",
+              Venue: "USA",
+              VenueName: "National Indoor Stadium",
+              SessionType: "EVE",
             },
             SessionName: [
               {
                 $: {
                   Language: "ENG",
-                  Value: "Session 1",
+                  Value: "Session 8",
                 },
               },
             ],
           },
           {
             $: {
-              SessionCode: "PBTH02",
-              StartDate: "2022-03-08T10:00:00+08:00",
-              EndDate: "2022-03-08T15:25:00+08:00",
-              Venue: "ZBT",
-              VenueName: "Zhangjiakou National Biathlon Centre",
-              Medal: "Y",
+              SessionCode: "PIH988",
+              StartDate: "2022-02-08T09:35:00+08:00",
+              EndDate: "2022-03-08T11:35:00+08:00",
+              Leadin: "0:00",
+              Venue: "NIS",
+              VenueName: "National Indoor Stadium",
+              SessionType: "MOR",
             },
             SessionName: [
               {
                 $: {
                   Language: "ENG",
-                  Value: "Session 2",
-                },
-              },
-            ],
-          },
-          {
-            $: {
-              SessionCode: "PBTH03",
-              StartDate: "2022-03-11T10:00:00+08:00",
-              EndDate: "2022-03-11T15:20:00+08:00",
-              Venue: "ZBT",
-              VenueName: "Zhangjiakou National Biathlon Centre",
-              Medal: "Y",
-            },
-            SessionName: [
-              {
-                $: {
-                  Language: "ENG",
-                  Value: "Session 3",
+                  Value: "Session 9",
                 },
               },
             ],
@@ -78,9 +58,7 @@ let xmlParsedBodyToJson = {
             $: {
               Code: "TRU-IHO-------------------3D------",
               PhaseType: "2",
-              ScheduleStatus: "UNSCHEDULED",
-              ActualStartDate: "2006+01:00",
-              ActualEndDate: "2006+01:00",
+              ScheduleStatus: "SCHEDULED",
               StartDate: "2022-02-17T12:00:00+08:00",
               HideStartDate: "N",
               EndDate: "2022-02-17T13:00:00+08:00",
@@ -89,7 +67,7 @@ let xmlParsedBodyToJson = {
               Location: "NIS",
               MediaAccess: "OPE",
               ModificationIndicator: "U",
-              SessionCode: "BOB05",
+              SessionCode:"BOB05"
             },
             ItemName: [
               {
@@ -128,6 +106,130 @@ let xmlParsedBodyToJson = {
               },
             ],
           },
+          {
+            $: {
+              Code: "TENMSINGLES-----------FNL-0001----",
+              PhaseType: "3",
+              UnitNum: "11",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2016-08-05T13:00:00+05:00",
+              HideStartDate: "Y",
+              EndDate: "2016-08-05T13:30:00+05:00",
+              HideEndDate: "Y",
+              Medal: "1",
+              Venue: "WEM",
+              Location: "TE0",
+              SessionCode: "TEN12",
+            },
+            StartText: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "followed by",
+                },
+              },
+            ],
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Singles Gold Medal Match",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "Wimbledon",
+                  LocationName: "Centre Court",
+                },
+              },
+            ],
+            StartList: [
+              {
+                Start: [
+                  {
+                    $: {
+                      SortOrder: "1",
+                      StartOrder: "1",
+                    },
+                    Competitor: [
+                      {
+                        $: {
+                          Code: "1051631",
+                          Type: "A",
+                          Organisation: "BLR",
+                        },
+                        Composition: [
+                          {
+                            Athlete: [
+                              {
+                                $: {
+                                  Code: "1051631",
+                                  Order: "1",
+                                },
+                                Description: [
+                                  {
+                                    $: {
+                                      GivenName: "Barry",
+                                      FamilyName: "Smith",
+                                      Gender: "M",
+                                      Organisation: "BLR",
+                                      BirthDate: "1983-07-23",
+                                      IFId: "397806",
+                                    },
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                  {
+                    $: {
+                      SortOrder: "2",
+                      StartOrder: "2",
+                    },
+                    Competitor: [
+                      {
+                        $: {
+                          Code: "1131104",
+                          Type: "A",
+                          Organisation: "USA",
+                        },
+                        Composition: [
+                          {
+                            Athlete: [
+                              {
+                                $: {
+                                  Code: "1131104",
+                                  Order: "1",
+                                },
+                                Description: [
+                                  {
+                                    $: {
+                                      GivenName: "Bobby",
+                                      FamilyName: "Jones",
+                                      Gender: "M",
+                                      Organisation: "USA",
+                                      BirthDate: "1969-10-23",
+                                      IFId: "573006",
+                                    },
+                                  },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
         ],
       },
     ],
@@ -135,43 +237,17 @@ let xmlParsedBodyToJson = {
 };
 
 
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-
-async function readJSONFile(path) {
-  const response = await fs.readFileSync(path, { encoding: 'utf-8' });
-  return JSON.parse(response);
-}
-
-
-async function eventsGenerator(payload) {
+ async function eventsGenerator(payload) {
   const today = new Date();
   const events = {
     OlympicsSession: [],
     OlympicsUnit: [],
   };
 
-const owgSports = await readJSONFile(
-    path.resolve(__dirname, './commonCodes/owgSports.json')
-  );
-  const pwgSports = await readJSONFile(
-    path.resolve(__dirname, './commonCodes/pwgSports.json')
-  );
   const competitionCode = payload["OdfBody"]["$"]["CompetitionCode"];
-  
-
-  const sportName =
-    competitionCode === 'OWG2022'
-      ? owgSports.find((sport) => sport.sportCode).sport
-      : pwgSports.find((sport) => sport.sportCode).sport;
-      
-     console.log(sportName);
-
   if (payload["OdfBody"]["Competition"][0]["Session"]) {
     payload["OdfBody"]["Competition"][0]["Session"].forEach((session) => {
-      sessionBody = {
+      sessionEvent = {
         publisherId: "d90972a3-65a5-447d-ae7b-084b8df9786d",
         clientEventId: session["$"]["SessionCode"],
         eventTypeCode: "OlympicsSession",
@@ -197,12 +273,13 @@ const owgSports = await readJSONFile(
         endDate: session["$"]["EndDate"],
         updateDate: today,
       };
-      events.OlympicsSession.push(sessionBody);
+      events.OlympicsSession.push(sessionEvent);
     });
   }
   if (payload["OdfBody"]["Competition"][0]["Unit"]) {
     payload["OdfBody"]["Competition"][0]["Unit"].forEach((unit) => {
-      unitBody = {
+      const gender = getValueOfGender(unit);
+      unitEvent = {
         publisherId: "d90972a3-65a5-447d-ae7b-084b8df9786d",
         clientEventId: unit["$"]["Code"],
         eventTypeCode: "OlympicsUnit",
@@ -210,21 +287,21 @@ const owgSports = await readJSONFile(
           name: unit["$"]["ScheduleStatus"],
         },
         eventBody: {
-            code: unit["$"]["Code"],
-            phaseType: unit["$"]["PhaseType"],
-            eventName: unit["ItemName"]
-              ? unit["ItemName"][0]["$"]["Value"]
-              : undefined,
-            sessionCode: unit["$"]["SessionCode"],
-            startDate: unit["$"]["StartDate"],
-            endDate: unit["$"]["EndDate"],
-            eventStatus: {
-              name: unit["$"]["ScheduleStatus"],
-            },
-            competition: {
-              code: competitionCode,
-              documentCode: payload["OdfBody"]["$"]["DocumentCode"],
-            },
+          code: unit["$"]["Code"],
+          phaseType: unit["$"]["PhaseType"],
+          eventName: unit["ItemName"]
+            ? unit["ItemName"][0]["$"]["Value"]
+            : undefined,
+          sessionCode: unit["$"]["SessionCode"],
+          startDate: unit["$"]["StartDate"],
+          endDate: unit["$"]["EndDate"],
+          eventStatus: {
+            name: unit["$"]["ScheduleStatus"],
+          },
+          competition: {
+            code: competitionCode,
+            documentCode: payload["OdfBody"]["$"]["DocumentCode"],
+          },
         },
         eventLocation: {
           venueName: unit["VenueDescription"]
@@ -238,38 +315,45 @@ const owgSports = await readJSONFile(
         endDate: unit["$"]["EndDate"],
         updateDate: today,
       };
-      if (unit["$"]["ScheduleStatus"] === "RESCHEDULED") {
-        unitBody.eventBody.endDate = unit["$"]["ActualEndDate"];
-        unitBody.endDate = unit["$"]["ActualEndDate"];
+      if(gender){
+        unitEvent.eventBody.gender = gender;
       }
-     else if (
+      if (unit["$"]["ScheduleStatus"] === "RESCHEDULED") {
+        unitEvent.eventBody.endDate = unit["$"]["ActualEndDate"];
+        unitEvent.endDate = unit["$"]["ActualEndDate"];
+      } else if (
         unit["$"]["ScheduleStatus"] !== "UNSCHEDULED" &&
         unit["$"]["ScheduleStatus"] !== "CANCELLED"
       ) {
-        unitBody.eventBody.endDate = unit["$"]["EndDate"];
-        unitBody.endDate = unit["$"]["EndDate"];
+        unitEvent.eventBody.endDate = unit["$"]["EndDate"];
+        unitEvent.endDate = unit["$"]["EndDate"];
       }
       if (unit["$"]["ScheduleStatus"] === "RESCHEDULED") {
-        unitBody.eventBody.startDate = unit["$"]["ActualStartDate"];
-        unitBody.startDate = unit["$"]["ActualStartDate"];
+        unitEvent.eventBody.startDate = unit["$"]["ActualStartDate"];
+        unitEvent.startDate = unit["$"]["ActualStartDate"];
       } else if (
         unit["$"]["ScheduleStatus"] !== "UNSCHEDULED" &&
         unit["$"]["ScheduleStatus"] !== "CANCELLED" &&
         unit["$"]["ScheduleStatus"] !== "POSTPONED"
       ) {
-        unitBody.eventBody.startDate = unit["$"]["StartDate"];
-        unitBody.startDate = unit["$"]["StartDate"];
-      } 
-      events.OlympicsUnit.push(unitBody);
+        unitEvent.eventBody.startDate = unit["$"]["StartDate"];
+        unitEvent.startDate = unit["$"]["StartDate"];
+      }
+      events.OlympicsUnit.push(unitEvent);
     });
-
-   
   }
   return events;
 }
 
+function getValueOfGender(unit){
+  try {
+   return unit["StartList"][0]["Start"][0]["Competitor"][0]["Composition"][0]["Athlete"][0]["Description"][0]["$"]["Gender"]
+  } catch (error) {
+    return undefined
+  }
+}
 // console.log(eventsGenerator(xmlParsedBodyToJson));
 
 let eventBody = eventsGenerator(xmlParsedBodyToJson);
-console.log(eventBody.OlympicsSession);
-// console.log(eventBody.OlympicsUnit);
+
+console.log(eventBody.OlympicsUnit);
