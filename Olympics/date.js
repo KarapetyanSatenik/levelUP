cd0992192a4e1156a5edb96854d4f8fc05f7c33b
KarapetyanSatenik <karapetyansatenik99@gmail.com>
2022-11-22 12:16:16 +0400

    sessions

--- Olympics/date.js
+++ Olympics/date.js
@@ -57,7 +57,513 @@ let xmlParsedBodyToJson = {
         Unit: [
           {
             $: {
-              Code: "CURXTEAM4---00000-----PREL000100--",
+              Code: "CURXTEAM4---00000-----PREL0001111--",
+              PhaseType: "2",
+              ScheduleStatus: "UNSCHEDULED",
+              StartDate: "2022-02-17T12:00:00+08:00",
+              HideStartDate: "N",
+              EndDate: "2022-02-17T13:00:00+08:00",
+              HideEndDate: "N",
+              Venue: "NIS",
+              Location: "NIS",
+              MediaAccess: "OPE",
+              ModificationIndicator: "U",
+              SessionCode: "BOB05",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Practice Men's Pre-Game SVK",
+                },
+              },
+              {
+                $: {
+                  Language: "CHI",
+                  Value: "Men's Practice Men's Pre-Game",
+                },
+              },
+            ],
+            ItemDescription: [
+              {
+                _: "<p>M SF1 (Away) MNA SVK</p>",
+                $: {
+                  Language: "ENG",
+                },
+              },
+              {
+                _: "<p>M SF (TBA) MNA</p>",
+                $: {
+                  Language: "SPA",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Indoor Stadium",
+                  LocationName: "National Indoor Stadium",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------CNCL000100--",
+              PhaseType: "3",
+              ScheduleStatus: "UNSCHEDULED",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "No competition",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------CNCL000200--",
+              PhaseType: "3",
+              ScheduleStatus: "UNSCHEDULED",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "No competition",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------CNCL000300--",
+              PhaseType: "3",
+              ScheduleStatus: "UNSCHEDULED",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "No competition",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------MEET000100--",
+              PhaseType: "9",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-04T14:00:00+08:00",
+              EndDate: "2022-03-04T15:00:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+              MediaAccess: "CLO",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Para Biathlon Team Captains' Meeting",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------MEET000200--",
+              PhaseType: "9",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-07T17:00:00+08:00",
+              EndDate: "2022-03-07T18:00:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+              MediaAccess: "CLO",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Para Biathlon Team Captains' Meeting",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------MEET000300--",
+              PhaseType: "9",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-10T16:00:00+08:00",
+              EndDate: "2022-03-10T17:00:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+              MediaAccess: "CLO",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Para Biathlon Team Captains' Meeting",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------MEET000400--",
+              PhaseType: "9",
+              ScheduleStatus: "UNSCHEDULED",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Para Biathlon Team Captains' Meeting",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------MEET000500--",
+              PhaseType: "9",
+              ScheduleStatus: "UNSCHEDULED",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Para Biathlon Team Captains' Meeting",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------MEET000600--",
+              PhaseType: "9",
+              ScheduleStatus: "UNSCHEDULED",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Para Biathlon Team Captains' Meeting",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTH-------------------MEET000700--",
+              PhaseType: "9",
+              ScheduleStatus: "UNSCHEDULED",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Para Biathlon Team Captains' Meeting",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----03030-----FNL-000100--",
+              PhaseType: "3",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-11T14:25:00+08:00",
+              EndDate: "2022-03-11T15:16:00+08:00",
+              HideEndDate: "Y",
+              Medal: "1",
+              Venue: "ZBT",
+              Location: "ZBT",
+              SessionCode: "PBTH03",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual VI",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----03030-----TRNO000100--",
+              PhaseType: "2",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-10T13:30:00+08:00",
+              EndDate: "2022-03-10T15:30:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual VI Off Train",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----03030-----VICTFLOWER--",
+              PhaseType: "6",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-11T15:15:00+08:00",
+              EndDate: "2022-03-11T15:19:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+              SessionCode: "PBTH03",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual VI Ven Cer",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----03030-----VICTMEDAL---",
+              PhaseType: "6",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-11T20:28:00+08:00",
+              EndDate: "2022-03-11T20:41:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+              SessionCode: "PMDL12",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual VI Vic Cer",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----09070-----FNL-000100--",
+              PhaseType: "3",
+              ScheduleStatus: "CANCELLED",
+              StartDate: "2022-03-11T12:40:00+08:00",
+              EndDate: "2022-03-11T13:27:00+08:00",
+              HideEndDate: "Y",
+              Medal: "1",
+              Venue: "ZBT",
+              Location: "ZBT",
+              SessionCode: "PBTH03",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual Standing",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----09070-----TRNO000100--",
+              PhaseType: "2",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-10T11:15:00+08:00",
+              EndDate: "2022-03-10T13:30:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual Standing Off Train",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----09070-----VICTFLOWER--",
+              PhaseType: "6",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-11T13:25:00+08:00",
+              EndDate: "2022-03-11T13:29:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+              SessionCode: "PBTH03",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual Standing Ven Cer",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----09070-----VICTMEDAL---",
+              PhaseType: "6",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-11T19:41:00+08:00",
+              EndDate: "2022-03-11T19:51:00+08:00",
+              Medal: "0",
+              Venue: "ZBT",
+              Location: "ZBT",
+              SessionCode: "PMDL12",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual Standing Vic Cer",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "BTHMLONG----12050-----FNL-000100--",
+              PhaseType: "3",
+              ScheduleStatus: "SCHEDULED",
+              StartDate: "2022-03-11T10:30:00+08:00",
+              EndDate: "2022-03-11T11:19:00+08:00",
+              HideEndDate: "Y",
+              Medal: "1",
+              Venue: "ZBT",
+              Location: "ZBT",
+              SessionCode: "PBTH03",
+            },
+            ItemName: [
+              {
+                $: {
+                  Language: "ENG",
+                  Value: "Men's Individual Sitting",
+                },
+              },
+            ],
+            VenueDescription: [
+              {
+                $: {
+                  VenueName: "National Biathlon Centre",
+                  LocationName: "National Biathlon Centre",
+                },
+              },
+            ],
+          },
+          {
+            $: {
+              Code: "CURXTEAM4---00000-----PREL002222--",
               PhaseType: "2",
               ScheduleStatus: "SCHEDULED",
               StartDate: "2022-02-17T12:00:00+08:00",
@@ -112,6 +618,7 @@ let xmlParsedBodyToJson = {
     ],
   },
 };
+'use strict';
 
 const fs = require('fs');
 const path = require('path');
@@ -119,13 +626,13 @@ const path = require('path');
 async function eventsGenerator(payload) {
   const events = {};
   const sports = await readJSONFile(
-    path.resolve(__dirname, './commonCodes/sports.json')
+    path.resolve(__dirname, '../commonCodes/sports.json')
   );
   const sportsSpecificCodes = await readJSONFile(
-    path.resolve(__dirname, './commonCodes/sportSpecificCodes.json')
+    path.resolve(__dirname, '../commonCodes/sportSpecificCodes.json')
   );
   const gender = await readJSONFile(
-    path.resolve(__dirname, './commonCodes/sportGenders.json')
+    path.resolve(__dirname, '../commonCodes/sportGenders.json')
   );
 
   const commonData = {
@@ -221,6 +728,13 @@ function generateUnitEvents(commonData, units) {
   const unitEvents = [];
 
   units.forEach((unit) => {
+    if (
+      unit['$']['ScheduleStatus'] === 'UNSCHEDULED' ||
+      unit['$']['ScheduleStatus'] === 'CANCELLED' ||
+      unit['$']['ScheduleStatus'] === 'POSTPONED'
+    ) {
+      return;
+    }
     const eventStage = getEventStage(
       commonData,
       unit['$']['Code'].substr(22, 4)
@@ -280,5 +794,5 @@ let eventBody = eventsGenerator(xmlParsedBodyToJson);
 
 eventBody.then((r) => {
   // console.log(r.OlympicsSession);
-  console.log(r.OlympicsUnit);
+  console.log(r.OlympicsUnit.length);
 });
